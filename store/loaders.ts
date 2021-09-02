9580e20bd8e237c158e18bc43bc239d43796937e
Galaad Moutoz <galaadmoutoz@gmail.com>
2021-09-02 23:32:24 +0200

    [dev] CollectionLoader test

--- store/loaders.ts
+++ store/loaders.ts
@@ -3,6 +3,7 @@ import { Module, VuexModule, Action } from "vuex-module-decorators";
 import firebase from "firebase/app";
 import { DocLoader, FirestoreData } from "@/extensions/vuex-firestore/src";
 import DummyDoc from "~/types/dummy-doc";
+import { CollectionLoader } from "@/extensions/vuex-firestore/lib";
 
 Vue.prototype.$dummyDoc = function (): DummyDoc {
   return (this as Vue).$store.state.loaders.dummyDoc as DummyDoc;
@@ -11,6 +12,16 @@ Vue.prototype.$dummyDoc = function (): DummyDoc {
 declare module "vue/types/vue" {
   interface Vue {
     $dummyDoc(): DummyDoc;
+  }
+}
+
+Vue.prototype.$dummyCollection = function (): DummyDoc {
+  return (this as Vue).$store.state.loaders.dummyCollection as DummyDoc;
+};
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $dummyCollection(): DummyDoc;
   }
 }
 
@@ -34,5 +45,17 @@ export default class SurveyModule extends VuexModule {
 
         if (!snapshot.exists) snapshot.ref.set(dummyDoc.clone());
       });
+  }
+
+  dummyCollection: DummyDoc[] = [];
+
+  @Action({ rawError: true })
+  @CollectionLoader("dummyCollection")
+  loadDummyCollection() {
+    return (docs: firebase.firestore.DocumentSnapshot[]) => {
+      const dummyCollection = docs.map((doc) => FirestoreData.fromSnapshot(DummyDoc, doc));
+
+      this.context.commit("setSurveyQuestions", dummyCollection);
+    };
   }
 }
